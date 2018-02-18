8e96b65896e31df1435da695354bb6095011a70a
Michael Beer <public@michael.beer.name>
2018-02-18 21:41:00 +0100

    Initial release

---
+++ OutlookSwissPTTimetable/RibbonTimetable.cs
@@ -0,0 +1,136 @@
+﻿using OutlookSwissPTTimetable.Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using Office = Microsoft.Office.Core;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+// TODO:  Führen Sie diese Schritte aus, um das Element auf dem Menüband (XML) zu aktivieren:
+
+// 1: Kopieren Sie folgenden Codeblock in die ThisAddin-, ThisWorkbook- oder ThisDocument-Klasse.
+
+//  protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+//  {
+//      return new Ribbon1();
+//  }
+
+// 2. Erstellen Sie Rückrufmethoden im Abschnitt "Menübandrückrufe" dieser Klasse, um Benutzeraktionen
+//    zu behandeln, z.B. das Klicken auf eine Schaltfläche. Hinweis: Wenn Sie dieses Menüband aus dem Menüband-Designer exportiert haben,
+//    verschieben Sie den Code aus den Ereignishandlern in die Rückrufmethoden, und ändern Sie den Code für die Verwendung mit dem
+//    Programmmodell für die Menübanderweiterung (RibbonX).
+
+// 3. Weisen Sie den Steuerelementtags in der Menüband-XML-Datei Attribute zu, um die entsprechenden Rückrufmethoden im Code anzugeben.  
+
+// Weitere Informationen erhalten Sie in der Menüband-XML-Dokumentation in der Hilfe zu Visual Studio-Tools für Office.
+
+
+namespace OutlookSwissPTTimetable
+{
+    [ComVisible(true)]
+    public class RibbonTimetable : Office.IRibbonExtensibility
+    {
+        private Office.IRibbonUI ribbon;
+
+        public RibbonTimetable()
+        {
+        }
+
+        #region IRibbonExtensibility-Member
+
+        public string GetCustomUI(string ribbonID)
+        {
+            string ribbonXML = String.Empty;
+
+            if (ribbonID == "Microsoft.Outlook.Explorer")
+            {
+                ribbonXML = GetResourceText("OutlookSwissPTTimetable.RibbonTimetable.xml");
+            }
+
+            return ribbonXML;
+        }
+
+        #endregion
+
+        #region Menübandrückrufe
+        //Erstellen Sie hier Rückrufmethoden. Weitere Informationen zum Hinzufügen von Rückrufmethoden finden Sie unter https://go.microsoft.com/fwlink/?LinkID=271226.
+
+        public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.ribbon = ribbonUI;
+        }
+
+        public void OnPlanJourneyButton(Office.IRibbonControl control)
+        {
+            // Get the Application object
+            Outlook.Application application = Globals.ThisAddIn.Application;
+            Outlook.MAPIFolder selectedFolder = application.ActiveExplorer().CurrentFolder;
+
+
+            string expMessage = "";
+            try
+            {
+                if (application.ActiveExplorer().Selection.Count > 0)
+                {
+                    Object selObject = application.ActiveExplorer().Selection[1];
+                    if (selObject is Outlook.AppointmentItem)
+                    {
+                        Outlook.AppointmentItem apptItem = (selObject as Outlook.AppointmentItem);
+                        PlanJourneyWindow pjw = new PlanJourneyWindow
+                        {
+                            Appointment = apptItem,
+                            MAPIFolder = selectedFolder
+                        };
+
+                        pjw.ShowDialog();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                expMessage = ex.Message;
+            }
+            if (expMessage != "")
+            {
+                MessageBox.Show(expMessage);
+            }
+
+        }
+
+        public Bitmap PlanJourneyButtonGetImage(Office.IRibbonControl control)
+        {
+            return Resources.B_T01_64;
+        }
+
+        #endregion
+
+        #region Hilfsprogramme
+
+        private static string GetResourceText(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string[] resourceNames = asm.GetManifestResourceNames();
+            for (int i = 0; i < resourceNames.Length; ++i)
+            {
+                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+                    {
+                        if (resourceReader != null)
+                        {
+                            return resourceReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ OutlookSwissPTTimetable/ThisAddIn.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using Office = Microsoft.Office.Core;
+using System.IO;
+
+namespace OutlookSwissPTTimetable
+{
+    public partial class ThisAddIn
+    {
+        public string DefaultStationsXMLFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DefaultStations.xml");
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            // Hinweis: Outlook löst dieses Ereignis nicht mehr aus. Wenn Code vorhanden ist, der 
+            //    muss ausgeführt werden, wenn Outlook heruntergefahren wird. Weitere Informationen finden Sie unter https://go.microsoft.com/fwlink/?LinkId=506785.
+        }
+
+        protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+        {
+            return new RibbonTimetable();
+        }
+
+        #region Von VSTO generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}

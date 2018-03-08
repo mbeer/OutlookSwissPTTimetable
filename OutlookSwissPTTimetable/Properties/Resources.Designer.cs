91c5ada3346fc44f21fdbef3756ca6febbe365ba
Michael Beer <public@michael.beer.name>
2018-03-08 18:43:23 +0100

    Added application icon

--- OutlookSwissPTTimetable/Properties/Resources.Designer.cs
+++ OutlookSwissPTTimetable/Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace OutlookSwissPTTimetable.Properties {
         }
         
         /// <summary>
+        ///   Sucht eine lokalisierte Ressource vom Typ System.Drawing.Icon ähnlich wie (Symbol).
+        /// </summary>
+        internal static System.Drawing.Icon B_T01 {
+            get {
+                object obj = ResourceManager.GetObject("B_T01", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Sucht eine lokalisierte Ressource vom Typ System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap B_T01_64 {
